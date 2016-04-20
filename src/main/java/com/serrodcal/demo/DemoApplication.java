6499e1b5ad89e65cf1bf25225c8c20efd4a07964
Sergio Rodríguez Calvo <serrodcal@MacBook-Pro-de-Sergio.local>
2016-04-20 02:36:36 +0200

    Change packages path name and apply plugin to idea

--- src/main/java/com/serrodcal/demo/DemoApplication.java
+++ src/main/java/com/serrodcal/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.serrodcal;
+package com.serrodcal.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/serrodcal/demo/DemoApplicationTests.java
+++ src/test/java/com/serrodcal/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.serrodcal;
+package com.serrodcal.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
